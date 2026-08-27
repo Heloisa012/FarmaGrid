9c3cd0684930b9b9061b84690396c1f64ea0a450
carol <carollanzoni1@gmail.com>
2026-08-27 16:57:42 -0300

    Api hospedada, url do render

--- FarmaGrid+_MOBILE/lib/config/api_config.dart
+++ FarmaGrid+_MOBILE/lib/config/api_config.dart
@@ -1,16 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  static const String baseUrlEmulador = 'http://10.0.2.2:8080';
-  static const String baseUrlCelularFisico = 'http://192.168.0.15:8080';
-  static const String baseUrlIosSimulator = 'http://localhost:8080';
-  static const String baseUrlPadraoWeb = 'http://localhost:8080';
-
-  static String get baseUrl {
-    const configurado = String.fromEnvironment('API_BASE_URL', defaultValue: '');
-
-    if (configurado.isNotEmpty) return configurado;
-    if (kIsWeb) return baseUrlPadraoWeb;
-    return baseUrlEmulador;
-  }
+  static const String baseUrl = 'https://farmagrid.onrender.com/';
 }
